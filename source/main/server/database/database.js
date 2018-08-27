325fc21329e8b93b0b34d91f4a32824d0843d661
Tomáš Rottenberg <frzerostbite@protonmail.com>
2018-08-27 11:28:42 +0200

    Migrate to PostgreSQL database.

--- source/main/server/database/database.js
+++ source/main/server/database/database.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 
 import Url from '../model/Url';
 
+const url = process.env.DATABASE_URL;
 const modules = { Url };
-const sequelize = new Sequelize('sqlite:build/database.db', { logging: false });
+const sequelize = new Sequelize(url, { logging: false });
 const model = Object.assign(
   {},
   ...Object.keys(modules)
